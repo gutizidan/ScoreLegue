da04185e886e50b9e919ee9f78cef1271fa8d68b
unknown <gutizidan@gmail.com>
2019-09-04 12:19:36 +0700

    new ndro

---
+++ app/src/main/java/com/example/scorelegue/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.scorelegue;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "AndroidClarified";
+    private GoogleSignInClient googleSignInClient;
+    private SignInButton googleSignInButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        googleSignInButton = findViewById(R.id.googleSignInButton);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 101);
+            }
+        });
+    }
+}
+
